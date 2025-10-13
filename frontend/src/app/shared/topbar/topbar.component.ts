ded3ebfa22ce1b44177524eb2eb697aa3da24955
Maximilian Boll <webmaster@myxotod.de>
2025-10-13 23:27:54 +0200

    Added dummy for profile menu

--- frontend/src/app/shared/topbar/topbar.component.ts
+++ frontend/src/app/shared/topbar/topbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, computed, inject } from '@angular/core';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { TopbarService } from './topbar.service';
 
 @Component({
@@ -6,9 +7,12 @@ import { TopbarService } from './topbar.service';
   selector: 'app-topbar',
   templateUrl: './topbar.component.html',
   styleUrl: './topbar.component.scss',
+  imports: [FaIconComponent],
 })
 export class TopbarComponent {
   private _topbarService = inject(TopbarService);
 
   pageTitle = computed(() => this._topbarService.pageTitle());
+
+  logout() {}
 }
